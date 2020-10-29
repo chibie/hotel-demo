602bc8153063adde40a28a4d771a3b7bc5be8305
Chibuotu Amadi <chibuotu@gmail.com>
2020-10-29 16:20:50 +0100

    feat(models): add rating field to property; price to 2dp

--- server/base/models.py
+++ server/base/models.py
@@ -3,9 +3,10 @@ from django.db import models
 
 class Property(models.Model):
 	name = models.CharField(max_length=150)
+    rating = models.CharField(max_length=10)
 	address = models.CharField(max_length=150)
 	state = models.CharField(max_length=50)
-	price = models.DecimalField(max_digits=8)
+	price = models.DecimalField(max_digits=8, decimal_places=2)
 
 	class Meta:
 		verbose_name_plural = 'Properties'
