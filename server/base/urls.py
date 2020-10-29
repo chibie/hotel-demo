8c3eb1207d7558840bf55805592d1eebe0a35a43
Chibuotu Amadi <chibuotu@gmail.com>
2020-10-29 16:50:12 +0100

    feat: urls and views
    - create booking
    - return books for a property

--- server/base/urls.py
+++ server/base/urls.py
@@ -1,1 +1,7 @@
-# urls
+from django.urls import path
+from .views import BookingCreate, PropertyBookedList
+
+urlpatterns = [
+	path('bookings/', BookingCreate.as_view(), name='bookings'),
+	path('properties/<int:id>/bookings/', PropertyBookedList.as_view(), name='properties_booked')
+]
--- server/base/views.py
+++ server/base/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, ListCreateAPIView
+from .models import Property, Booking
+from .serializers import PropertySerializer, BookingSerializer
 
-# views
+
+class BookingCreate(ListCreateAPIView):
+	"""
+	get:
+	Return all instances of the Booking model.
+
+	post:
+	Create a new Booking instance.
+	"""
+	queryset = Booking.objects.all()
+	serializer_class = BookingSerializer
+
+
+class PropertyBookedList(ListAPIView):
+	"""
+	Return a list of all Bookings corresponding to a Property.
+	"""
+    serializer_class = BookingSerializer
+
+	def get_queryset(self):
+		queryset = Property.objects.get(id=self.kwargs['id']).booking.all()
+		return queryset
+
