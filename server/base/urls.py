bb32a281aa585753e112bb3bd4f88f81cecf2214
Chibuotu Amadi <chibuotu@gmail.com>
2020-10-29 17:08:34 +0100

    feat: add view and url to fetch properties

--- server/base/urls.py
+++ server/base/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import BookingCreate, PropertyBookedList
+from .views import PropertyList, BookingCreate, PropertyBookedList
 from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
+	path('properties/', PropertyList.as_view(), name='properties'),
 	path('bookings/', BookingCreate.as_view(), name='bookings'),
 	path('properties/<int:id>/bookings/', PropertyBookedList.as_view(), name='properties_booked'),
-    path('docs/', include_docs_urls(title='Properties API Documentation'), name='docs')
+	path('docs/', include_docs_urls(title='Properties API Documentation'), name='docs')
 ]
--- server/base/views.py
+++ server/base/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
+from django.conf import settings
 from rest_framework.generics import ListAPIView, ListCreateAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Property, Booking
 from .serializers import PropertySerializer, BookingSerializer
+from .utils.fetch_hotels import fetch_hotels
+
+
+class PropertyList(APIView):
+	"""
+	Return all instances of the Property model.
+	If URL contains an `at` query string, return results from HERE Places API.
+	"""
+	def get(self, request):
+		if 'at' in self.request.GET:
+			lat, long = self.request.GET.get('at').split(',')
+			hotels = fetch_hotels(lat, long, 'hotel', 5, settings.HERE_API_KEY)
+			return Response(hotels)
+		else:
+			properties = Property.objects.all()
+			data = PropertySerializer(properties, many=True).data
+			return Response(data)
 
 
 class BookingCreate(ListCreateAPIView):
