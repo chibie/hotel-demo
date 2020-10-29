6e7c34a23f09325df8c6d94c83688cd8c7a29a80
Chibuotu Amadi <chibuotu@gmail.com>
2020-10-29 16:23:39 +0100

    feat(models): add booking model

--- server/base/models.py
+++ server/base/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Property(models.Model):
 	name = models.CharField(max_length=150)
-    rating = models.CharField(max_length=10)
+	rating = models.CharField(max_length=10)
 	address = models.CharField(max_length=150)
 	state = models.CharField(max_length=50)
 	price = models.DecimalField(max_digits=8, decimal_places=2)
@@ -11,3 +11,18 @@ class Property(models.Model):
 	class Meta:
 		verbose_name_plural = 'Properties'
 
+	def __str__(self):
+		return f'{self.name}'
+
+
+class Booking(models.Model):
+	first_name = models.CharField(max_length=150)
+	last_name = models.CharField(max_length=150)
+	email = models.EmailField(max_length=150, unique=True)
+	property_booked = models.ForeignKey(
+        Property, on_delete=models.CASCADE, related_name='booking')
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		verbose_name_plural = 'Bookings'
