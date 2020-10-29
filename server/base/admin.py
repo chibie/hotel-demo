22444e5fda9a6aa4d14de8bf8bb345bbb4221280
Chibuotu Amadi <chibuotu@gmail.com>
2020-10-29 16:39:37 +0100

    feat(admin): register property & booking models

--- server/base/admin.py
+++ server/base/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Property, Booking
 
-# admin
+
+class PropertyAdmin(admin.ModelAdmin):
+	list_display = ('name', 'id', 'address')
+	list_filter = ('name', 'state')
+	search_fields = ('name', 'state')
+
+
+class BookingAdmin(admin.ModelAdmin):
+	list_display = ('first_name', 'id', 'last_name', 'property_booked', 'created')
+	list_filter = ('first_name', 'last_name', 'property_booked')
+	search_fields = ('first_name', 'last_name', 'property_booked')
+	raw_id_fields = ('property_booked',)
+
+
+admin.site.register(Property, PropertyAdmin)
+admin.site.register(Booking, BookingAdmin)
