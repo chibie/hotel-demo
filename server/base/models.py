f4d0aef73753fd25594bfdff2db767c70f81a4cf
Chibuotu Amadi <chibuotu@gmail.com>
2020-10-29 16:19:13 +0100

    feat(models): add property model

--- server/base/models.py
+++ server/base/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# models
+
+class Property(models.Model):
+	name = models.CharField(max_length=150)
+	address = models.CharField(max_length=150)
+	state = models.CharField(max_length=50)
+	price = models.DecimalField(max_digits=8)
+
+	class Meta:
+		verbose_name_plural = 'Properties'
+
