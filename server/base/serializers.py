d7671927b99b3702a555f0d5c85d9073ed157a77
Chibuotu Amadi <chibuotu@gmail.com>
2020-10-29 16:36:57 +0100

    feat(serializers): add property and booking

--- server/base/serializers.py
+++ server/base/serializers.py
@@ -1,3 +1,14 @@
 from rest_framework import serializers
+from .models import Property, Booking
 
-# serializers
+
+class PropertySerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Property
+		fields = '__all__'
+
+
+class BookingSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Booking
+		fields = '__all__'
