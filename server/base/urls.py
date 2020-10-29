e968acf409b6f0c2b4d823e58833069039c6a5a3
Chibuotu Amadi <chibuotu@gmail.com>
2020-10-29 16:51:34 +0100

    feat(urls): include docs

--- server/base/urls.py
+++ server/base/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import BookingCreate, PropertyBookedList
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
 	path('bookings/', BookingCreate.as_view(), name='bookings'),
-	path('properties/<int:id>/bookings/', PropertyBookedList.as_view(), name='properties_booked')
+	path('properties/<int:id>/bookings/', PropertyBookedList.as_view(), name='properties_booked'),
+    path('docs/', include_docs_urls(title='Properties API Documentation'), name='docs')
 ]
